8ecd6e701445093ee4e1f5be04d4bcd6cabd08f8
Jalal Amourgha <myproo100@gmail.com>
2024-02-08 17:45:31 +0100

    new navbar

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,6 @@ let switcherButtons = document.querySelectorAll(".color-switcher button");
 let root = document.querySelector(":root");
 let changeModeBtn = document.getElementById("changeModeBtn");
 let switcher = document.querySelectorAll(".buttons a");
-const li = document.querySelectorAll(".link");
-const sections = document.querySelectorAll("section");
-let scroller = document.getElementById("scroller");
-let height =
-  document.documentElement.scrollHeight - document.documentElement.clientHeight;
-let scrollBtn = document.getElementsByClassName("scroll-btn")[0];
 
 let projectsBoxs = Array.from(document.getElementsByClassName("all"));
 // this variable allos us to know if the switcher box visible or not
@@ -48,15 +42,13 @@ switcherButtons.forEach((button) => {
 // Dark Mode function
 function changeMode() {
   if (mode) {
-    changeModeBtn.classList.remove("fa-moon");
-    changeModeBtn.classList.add("fa-sun");
+    changeModeBtn.style.transform = "rotateZ(180deg)";
     root.style.setProperty("--bg-main", "#191d2b");
     root.style.setProperty("--bg-second", "white");
     root.style.setProperty("--input-bg-color", "#2c2d34");
     mode = false;
   } else {
-    changeModeBtn.classList.add("fa-moon");
-    changeModeBtn.classList.remove("fa-sun");
+    changeModeBtn.style.transform = "rotateZ(0deg)";
     root.style.setProperty("--bg-main", "white");
     root.style.setProperty("--bg-second", "#191d2b");
     root.style.setProperty("--input-bg-color", "#e2e2e2 ");
@@ -85,21 +77,10 @@ function manageImgs() {
     el.style.display = "block";
   });
 }
+
 function displayReactBox() {
   document.getElementById("react").style.display = "block";
 }
-
-// show the arrow top button
-window.addEventListener("scroll", () => {
-  let scrollTop = document.documentElement.scrollTop;
-  scroller.style.width = `${(scrollTop / height) * 100}%`;
-});
-
-window.onscroll = function () {
-  this.scrollY > 800
-    ? (scrollBtn.style.display = "block")
-    : (scrollBtn.style.display = "none");
-};
 
 function scrollBack() {
   window.scrollTo({
