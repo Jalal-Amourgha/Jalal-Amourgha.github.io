3a85ec023db82178debc21ee653adf09bfff1359
Jalal Amourgha <myproo100@gmail.com>
2024-07-14 16:41:07 +0100

    custom demoin

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({ base: "/Portfolio/", plugins: [react()] });
+export default defineConfig({
+  base: "/jalal-amourgha.github.io/",
+  plugins: [react()],
+});
